ee4647e1c753138986c1b7f4da046b75ed2bd8df
stegano <steganowork@naver.com>
2021-04-27 23:31:11 +0900

    'addResult'

--- app/src/main/java/com/stegano/bmicalc/MainActivity.kt
+++ app/src/main/java/com/stegano/bmicalc/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.stegano.bmicalc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.calc)
+        val inputUserWeight = findViewById<EditText>(R.id.inputUserWeight)
+        val inputUserHeight = findViewById<EditText>(R.id.inputUserHeight)
+
+        button?.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("weight", inputUserWeight.text.toString())
+            intent.putExtra("height", inputUserHeight.text.toString())
+
+            inputUserWeight.setText("")  // 입력값 초기화
+            inputUserHeight.setText("")
+
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/stegano/bmicalc/ResultActivity.kt
+++ app/src/main/java/com/stegano/bmicalc/ResultActivity.kt
@@ -2,10 +2,39 @@ package com.stegano.bmicalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val weight = intent.getStringExtra("weight")
+        val height = intent.getStringExtra("height")
+
+        val bmi = Integer.parseInt(weight) / Math.pow(Integer.parseInt(height) / 100.0, 2.0)
+        Toast.makeText(this, "결과가 나왔습니다.", Toast.LENGTH_SHORT).show()
+
+        when {
+            bmi >= 35 -> {
+                resultTextView.text = "고도 비만"
+                resultImageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_24)
+            }
+            bmi >= 30 -> {
+                resultTextView.text = "2단계 비만"
+                resultImageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_24)
+            }
+            bmi >= 25 -> {
+                resultTextView.text = "1단계 비만"
+                resultImageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_24)
+            }
+            bmi >= 23 -> resultTextView.text = "과체중"
+            bmi >= 18.5 -> resultTextView.text = "정상"
+            else -> {
+                resultTextView.text = "저체중"
+                resultImageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_24)
+            }
+        }
     }
 }
