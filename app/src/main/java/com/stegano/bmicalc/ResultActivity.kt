9c37f6b35258de06758c4c48062f8bca7e558e8c
stegano <steganowork@naver.com>
2021-04-28 20:49:04 +0900

    'update'

--- app/src/main/java/com/stegano/bmicalc/ResultActivity.kt
+++ app/src/main/java/com/stegano/bmicalc/ResultActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_result.*
+import kotlin.math.pow
+import kotlin.math.roundToInt
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,26 +15,29 @@ class ResultActivity : AppCompatActivity() {
         val weight = intent.getStringExtra("weight")
         val height = intent.getStringExtra("height")
 
-        val bmi = Integer.parseInt(weight) / Math.pow(Integer.parseInt(height) / 100.0, 2.0)
-        Toast.makeText(this, "결과가 나왔습니다.", Toast.LENGTH_SHORT).show()
+        val bmi = Integer.parseInt(weight) / (Integer.parseInt(height) / 100.0).pow(2.0)
+        Toast.makeText(this, "결과가 나왔습니다. bmi : ${bmi.roundToInt()}", Toast.LENGTH_SHORT).show()
 
         when {
             bmi >= 35 -> {
-                resultTextView.text = "고도 비만"
+                resultTextView.text = "고도 비만입니다."
                 resultImageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_24)
             }
             bmi >= 30 -> {
-                resultTextView.text = "2단계 비만"
+                resultTextView.text = "2단계 비만입니다."
                 resultImageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_24)
             }
             bmi >= 25 -> {
-                resultTextView.text = "1단계 비만"
+                resultTextView.text = "1단계 비만입니다."
                 resultImageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_24)
             }
-            bmi >= 23 -> resultTextView.text = "과체중"
-            bmi >= 18.5 -> resultTextView.text = "정상"
+            bmi >= 23 -> {
+                resultTextView.text = "과체중입니다."
+                resultImageView.setImageResource(R.drawable.ic_baseline_sentiment_dissatisfied_24)
+            }
+            bmi >= 18.5 -> resultTextView.text = "정상입니다."
             else -> {
-                resultTextView.text = "저체중"
+                resultTextView.text = "저체중입니다."
                 resultImageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_24)
             }
         }
